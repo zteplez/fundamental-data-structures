90ffae2a4d135ae91aa9ce148c1788511da2679a
zteplez <asibrahim98@gmail.com>
2025-03-29 23:43:23 +0300

    Add constructor and insert

--- CustomLinkedList.cs
+++ CustomLinkedList.cs
@@ -2,17 +2,38 @@ using System;
 
 namespace FundamentalDataStructures;
 
-public class LinkedList
+public class CustomLinkedList<T>
 {
     internal class Node<T>{
         T value;
         Node<T>? nextNode;
         Node<T>? prevNode;
+        public Node<T> NextNode {get => nextNode;}
         public Node(T value)
         {
             this.value = value;
             nextNode = null;
             prevNode = null;
+        }
+    }
+
+    Node<T>? firstNode;
+    Node<T>? lastNode;
+    int count;
+    public CustomLinkedList()
+    {
+        firstNode = null;
+        lastNode = null;
+    }
+    public void Insert(T value){
+        if(firstNode == null){
+            firstNode = new Node<T>(value);
+            Console.WriteLine("New node created on head.");
+        }else{
+            Node<T> iterator = firstNode;
+            while(iterator.NextNode != null){
+                
+            }
         }
     }
 }
