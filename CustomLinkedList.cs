5d9712e77147efbb035a68ddc7b8bda831b458f9
zteplez <asibrahim98@gmail.com>
2025-03-30 16:00:36 +0300

    Add Travese and Remove methods

--- CustomLinkedList.cs
+++ CustomLinkedList.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace FundamentalDataStructures;
 
-public class CustomLinkedList<T>
+public class CustomLinkedList<T> 
 {
-    internal class Node<T>{
-        T value;
-        Node<T>? nextNode;
-        Node<T>? prevNode;
-        public Node<T> NextNode {get => nextNode;}
+    internal class Node<T>
+    {
+        public T value;
+        public Node<T>? nextNode;
+        public Node<T>? prevNode;
         public Node(T value)
         {
             this.value = value;
@@ -25,15 +25,83 @@ public class CustomLinkedList<T>
         firstNode = null;
         lastNode = null;
     }
-    public void Insert(T value){
-        if(firstNode == null){
+    public void Insert(T value)
+    {
+        if (firstNode == null)
+        {
             firstNode = new Node<T>(value);
+            lastNode = firstNode;
             Console.WriteLine("New node created on head.");
-        }else{
+        }
+        else
+        {
             Node<T> iterator = firstNode;
-            while(iterator.NextNode != null){
-                
+            while (iterator.nextNode != null)
+            {
+                iterator = iterator.nextNode;
             }
+            Node<T> newNode = new Node<T>(value);
+            iterator.nextNode = newNode;
+            newNode.prevNode = iterator;
+            lastNode = newNode;
+            Console.WriteLine("New node created.");
+        }
+    }
+    public void Remove(T value)
+    {
+        if (firstNode != null)
+        {
+            Node<T> iterator = firstNode;
+            while(iterator != null){
+                if(EqualityComparer<T>.Default.Equals(iterator.value, value)){
+                    Console.WriteLine("Element found.");
+                    if(iterator == firstNode){
+                        firstNode = iterator.nextNode;
+                        iterator.prevNode = null;
+                        iterator = null;
+                    }else if(iterator == lastNode){
+                        lastNode = iterator.prevNode;
+                        lastNode.nextNode = null;
+                    }else{
+                        // Implement this.
+                    }
+                }
+            }
+        }
+    }
+    public void TraverseFromFirst()
+    {
+        if (firstNode != null)
+        {
+            Node<T> iterator = firstNode;
+            while (iterator.nextNode != null)
+            {
+                Console.WriteLine($"Value {iterator.value}");
+                iterator = iterator.nextNode;
+            }
+            Console.WriteLine($"Last node value {iterator.value}");
+        }
+        else
+        {
+            Console.WriteLine("Empty list.");
+        }
+
+    }
+    public void TraverseFromLast()
+    {
+        if (lastNode != null)
+        {
+            Node<T> iterator = lastNode;
+            while (iterator.nextNode != null)
+            {
+                Console.WriteLine($"Value {iterator.value}");
+                iterator = iterator.nextNode;
+            }
+            Console.WriteLine($"First node value {iterator.value}");
+        }
+        else
+        {
+            Console.WriteLine("Empty list.");
         }
     }
 }
