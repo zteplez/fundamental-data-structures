2e83167ba56b3b3f9a53cdb43517dbae2daeba5e
zteplez <asibrahim98@gmail.com>
2025-05-01 17:01:53 +0300

    [feat]: Complete base stack props and methods

--- src/CustomStack.cs
+++ src/CustomStack.cs
@@ -1,26 +1,85 @@
 using System;
+using System.Linq.Expressions;
 
 namespace FundamentalDataStructures.src;
 
 public class CustomStack<T>
 {
-    CustomList<T> list;
+    T[] stack;
     int count;
     int top;
+    const int BASE_CAPACITY = 10;
 
-    public CustomStack(){
-        list = new CustomList<T>();
-        count = 0; 
+    public CustomStack()
+    {
+        stack = new T[BASE_CAPACITY];
+        count = 0;
         top = -1;
     }
 
-    public void Push(){}
-    public void Pop(){}
-    public T? Peek(){
+    public void Push(T element)
+    {
+        if (count == stack.Length)
+        {
+            Console.WriteLine("Stack is full. Its gonna dynamically grow.");
+            GrowStack();
+        }
+
+        stack[++top] = element;
+        count++;
+    }
+    public void Pop()
+    {
+        if (top != -1)
+        {
+            stack[top] = default(T);
+            top--;
+            count--;
+            return;
+        }
+        Console.WriteLine("Empty stack.");
+    }
+    public T? Peek()
+    {
+        if (top != -1)
+        {
+
+            return stack[top];
+        }
         return default;
     }
-    public bool IsEmpty(){
+    public bool IsEmpty()
+    {
         return count == 0 ? true : false;
     }
+    public int Size() { return count; }
+    public void Clear()
+    {
+        for (int i = 0; i < stack.Length; i++)
+        {
+            stack[i] = default(T);
+        }
+        top = -1;
+        count = 0;
+    }
+    public override string ToString()
+    {
+        if (IsEmpty()) return "Stack is empty.";
 
+        string result = "";
+        for (int i = top; i >= 0; i--)
+        {
+            result += stack[i] + Environment.NewLine;
+        }
+        return result;
+    }
+
+    public void GrowStack()
+    {
+        int newCapacity = stack.Length * 2;
+        T[] newArr = new T[newCapacity];
+        Array.Copy(stack, newArr, stack.Length);
+        stack = newArr;
+
+    }
 }
