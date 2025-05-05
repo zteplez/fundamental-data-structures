9b57777bd5c3d4fba71d57be04d1d0e69b52a411
zteplez <asibrahim98@gmail.com>
2025-05-05 12:20:20 +0300

    [feat]: Create in-order printing method
    * Prints inorder calling InOrderTraversal method

--- src/CustomBinarySearchTree.cs
+++ src/CustomBinarySearchTree.cs
@@ -1,6 +1,7 @@
 class CustomBinarySearchTree
 {
-    internal class Node{
+    internal class Node
+    {
         internal Node? left;
         internal Node? right;
         internal int value;
@@ -16,63 +17,112 @@ class CustomBinarySearchTree
     {
         root = new Node(rootValue);
     }
-    public void CreateNode(int val){
+    public void CreateNode(int val)
+    {
         Node newNode = new Node(val);
-        
-        if(root == null){
+
+        if (root == null)
+        {
             root = newNode;
             return;
         }
         InsertNode(newNode, root);
     }
-    public void InsertNode(Node newNode, Node currNode){
-       if(newNode.value < currNode.value){
-            if(currNode.left == null){
+    public void InsertNode(Node newNode, Node currNode)
+    {
+        if (newNode.value < currNode.value)
+        {
+            if (currNode.left == null)
+            {
                 currNode.left = newNode;
                 return;
             }
             InsertNode(newNode, currNode.left);
         }
-        else if(newNode.value > currNode.value){
-            if(currNode.right == null){
+        else if (newNode.value > currNode.value)
+        {
+            if (currNode.right == null)
+            {
                 currNode.right = newNode;
                 return;
             }
             InsertNode(newNode, currNode.right);
-        }else{
+        }
+        else
+        {
             currNode.left = newNode; // What should I do when values are equal ?
         }
     }
-    public Node Find(int targetValue){
-        if(root == null){
+    public Node Find(int targetValue)
+    {
+        if (root == null)
+        {
             Console.WriteLine("Empty tree.");
             return null;
         }
 
         Node targetNode = FindNode(targetValue, root);
-        if(targetNode == null){
+        if (targetNode == null)
+        {
             Console.WriteLine("Cannot find target node.");
             return null;
-        }else{
+        }
+        else
+        {
             return targetNode;
         }
     }
-    private Node FindNode(int targetValue, Node currentNode){
-        if(targetValue < currentNode.value){
-            if(currentNode.left != null){
+    private Node FindNode(int targetValue, Node currentNode)
+    {
+        if (targetValue < currentNode.value)
+        {
+            if (currentNode.left != null)
+            {
                 FindNode(targetValue, currentNode.left);
-            }else{
+            }
+            else
+            {
                 return null;
             }
-        }else if(targetValue > currentNode.value){
-            if(currentNode.right != null){
+        }
+        else if (targetValue > currentNode.value)
+        {
+            if (currentNode.right != null)
+            {
                 FindNode(targetValue, currentNode.right);
-            }else{
+            }
+            else
+            {
                 return null;
             }
-        }else{
+        }
+        else
+        {
             return currentNode;
         }
         return null;
+    }
+    public void PrintInOrder()
+    {
+        if (root == null)
+        {
+            Console.WriteLine("Tree is empty.");
+            return;
+        }
+        InOrderTraversal(root);
+    }
+    public void InOrderTraversal(Node currentNode)
+    {
+        if (currentNode.left != null)
+        {
+            InOrderTraversal(currentNode.left);
+
+        }
+        Console.WriteLine("Value -> " + currentNode.value);
+
+        if (currentNode.right != null)
+        {
+            InOrderTraversal(currentNode.right);
+        }
     }
 }
