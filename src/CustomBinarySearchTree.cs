52afc597a3938c23321cbe805845f05d0ccdfbf1
zteplez <asibrahim98@gmail.com>
2025-05-05 11:59:17 +0300

    [feat]: Add Insert method
    * Add node creation method
    * Add recursive insert method
    * Change node accessor private to internal

--- src/CustomBinarySearchTree.cs
+++ src/CustomBinarySearchTree.cs
@@ -1,12 +1,12 @@
 class CustomBinarySearchTree
 {
-    class Node{
-        Node? left;
-        Node? right;
-        int root;
-        public Node(int value)
+    internal class Node{
+        internal Node? left;
+        internal Node? right;
+        internal int value;
+        public Node(int val)
         {
-            root = value;
+            value = val;
             left = null;
             right = null;
         }
@@ -16,7 +16,31 @@ class CustomBinarySearchTree
     {
         root = new Node(rootValue);
     }
-    public void Insert(){
+    public void CreateNode(int val){
+        Node newNode = new Node(val);
         
+        if(root == null){
+            root = newNode;
+            return;
+        }
+        InsertNode(newNode, root);
+    }
+    public void InsertNode(Node newNode, Node currNode){
+       if(newNode.value < currNode.value){
+            if(currNode.left == null){
+                currNode.left = newNode;
+                return;
+            }
+            InsertNode(newNode, currNode.left);
+        }
+        else if(newNode.value > currNode.value){
+            if(currNode.right == null){
+                currNode.right = newNode;
+                return;
+            }
+            InsertNode(newNode, currNode.right);
+        }else{
+            currNode.left = newNode; // What should I do when values are equal ?
+        }
     }
 }
