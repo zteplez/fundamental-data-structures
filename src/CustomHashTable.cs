24645004c19f3e6e53052c5e610dc8d32b92d40c
zteplez <asibrahim98@gmail.com>
2025-05-13 21:42:49 +0300

    [feat]: Add print table function
    * Add ToString for node class
    * Print table prints every list
    * Print list table is print every element in list

--- src/CustomHashTable.cs
+++ src/CustomHashTable.cs
@@ -5,12 +5,16 @@ public class CustomHashTable
     // Example key-value pair -> "ibrahim as", 201, <string,int>
     public class Node
     {
-        string key;
-        int value;
+        public string key;
+        public int value;
         public Node(string key, int value)
         {
             this.key = key;
             this.value = value;
+        }
+        public override string ToString()
+        {
+            return $"Key: {key}, Value: {value}";
         }
     }
     CustomList<Node>[] buckets;
@@ -23,13 +27,37 @@ public class CustomHashTable
             buckets[i] = new CustomList<Node>();
         }
     }
-    public int Hash(string key){
+    public int Hash(string key)
+    {
         int hash = 0;
-        for(int i = 0; i < key.Length; i++){
+        for (int i = 0; i < key.Length; i++)
+        {
             hash += key[i];
         }
         Console.WriteLine("hash -> " + hash % 13);
         return hash % 13;
+    }
+    public void Put(string key, int value)
+    {
+        Node newNode = new Node(key, value);
+        int index = Hash(key);
+        buckets[index].Add(newNode); 
+    }
+    public void PrintTable()
+    {
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            Console.Write(i + 1 + "Bucket -> ");
+            PrintListTable(buckets[i]);
+            Console.WriteLine("\n\n");
+        }
+    }
+    public void PrintListTable(CustomList<Node> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list.Get(i).ToString());
+        }
     }
 
 }
