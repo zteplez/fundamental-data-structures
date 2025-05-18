d113769759640c9271b8b69f8a251498b2d71040
zteplez <asibrahim98@gmail.com>
2025-05-18 15:30:57 +0300

    [feat]: Add enqueue method
    * Takes value and priority parameter, creates node
    * Checks if array empty than creates node and assigns in array
    * Calls bubble up to protect heap order

--- src/CustomPriorityQueue.cs
+++ src/CustomPriorityQueue.cs
@@ -7,7 +7,7 @@ public class CustomPriorityQueue
     public class Node
     {
         string? value;
-        int priority;
+        public int priority;
         public Node(string value, int priority)
         {
             this.value = value;
@@ -27,8 +27,26 @@ public class CustomPriorityQueue
             if (minHeap[i] == null)
             {
                 minHeap[i] = new Node(value, priority);
-                break;
+                BubbleUp(i);
+                Console.WriteLine("Add element.");
+                return;
             }
+        }
+        Console.WriteLine("Element couldn't add.");
+    }
+    public void BubbleUp(int index)
+    {
+        if (index == 0) return;
+
+        int parentIndex = (index - 1) / 2;
+        Node parent = minHeap[parentIndex];
+        Node currentElement = minHeap[index];
+
+        if (parent.priority > currentElement.priority)
+        {
+            minHeap[parentIndex] = currentElement;
+            minHeap[index] = parent;
+            BubbleUp(parentIndex);
         }
     }
     public void BuildHeap()
@@ -47,15 +65,13 @@ public class CustomPriorityQueue
         Console.Write($"Root -> {minHeap[0]}");
         Console.Write($"Left -> {minHeap[1]}");
         Console.Write($"Right -> {minHeap[2]}");
-        
+
         int parent = (i - 1) / 2;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
     }
 }
-
 /*
 Parent: i → (i - 1) / 2
 Left child: i → 2i + 1
-Right child: i → 2i + 2
-*/
+Right child: i → 2i + 2*/
